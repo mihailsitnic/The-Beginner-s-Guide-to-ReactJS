b82c56e329812d443db71d6fe83f9366e22bff84
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-12-29 19:42:23 +0200

    12-egghead-egghead-use-class-components-with-react

--- src/App.js
+++ src/App.js
@@ -2,40 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class StopWatch extends React.Component {
-  state = {lapse: 0, running: false}
-  handleRunClick = () => {
-    this.setState(state => {
-      if(state.running) {
-        clearInterval(this.timer)
-      } else {
-        const startTime = Date.now() - this.state.lapse
-        this.timer = setInterval(() => {
-          this.setState({lapse: Date.now() - startTime})
-        })
-      }
-      return {running: !state.running}
-    })
-  }
-  handleClearClick = () => {
-    clearInterval(this.timer)
-    this.setState({lapse: 0, running: false})
-  }
-  render() {
-    const {lapse, running} = this.state
-    return (
-      <div>
-        <label>{lapse}ms</label>
-        <button onClick={this.handleRunClick}>{running ? 'Stop' : 'Start'}</button>
-        <button onClick={this.handleClearClick}>Clear</button>
-      </div>
-    )
-  }
-}
-
-const element = <StopWatch />
 
 class App extends Component {
+  state = {count: 0}
+  handleClick = () => {
+    this.setState(({count}) => ({
+      count: count + 1,
+    }))
+  }
   render() {
     return (
       <div className="App">
@@ -43,7 +17,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        {element}
+        <button onClick={this.handleClick}>
+          {this.state.count}
+        </button>
       </div>
     );
   }
