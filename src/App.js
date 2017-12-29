0a833426480996df60d12bc3b6f5129fb077bcef
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-12-29 16:45:23 +0200

    08-egghead-react-style-react-components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function Message({message}) {
-  if (!message) {
-    return <div>No Message</div>
-  }
-  return <div>{message}</div>
+function Box({
+  style,
+  size,
+  className = '',
+  ...rest
+}) {
+  const sizeClassName = size ? `box-${size}` : '';
+  return(
+    <div
+      className={`box ${className} ${sizeClassName}`}
+      style={{padding: 20, ...style}}
+      {...rest}
+    />
+  )
 }
+
+const element = (
+  <div>
+    <Box
+      size='small'
+      style={{backgroundColor: 'lightblue'}}>Small Box</Box>
+    <Box
+      size='medium'
+      style={{backgroundColor: 'pink'}}>Medium Box</Box>
+    <Box
+      size='large'
+      style={{backgroundColor: 'orange'}}>Large Box</Box>
+  </div>
+)
 
 class App extends Component {
   render() {
@@ -17,7 +40,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Message message={null} />
+        {element}
       </div>
     );
   }
