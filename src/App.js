68e51a262eade91d74f1a8941d94b43b956f2847
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-12-30 21:51:57 +0200

    15-egghead-egghead-make-dynamic-forms-with-react

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class NameForm extends React.Component {
+  state = {error: null}
   handleSubmit = event => {
     event.preventDefault()
+    const value = event.target.elements.username.value;
+    const error = this.props.getErrorMessage(value);
+    if (error) {
+      alert(`error: ${error}`);
+    } else {
+      alert(`succsess: ${value}`);
+    }
     console.log({target: event.target});
     console.log(event.target[0].value);
     console.log(event.target.elements.username.value);
-    console.log(this.inputNode.value);
   }
+
+  hanleChange = (event) => {
+    const {value} = event.target
+    this.setState({
+      error: this.props.getErrorMessage(value),
+    })
+  }
+
   render() {
+    const {error} = this.state
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
@@ -18,10 +34,11 @@ class NameForm extends React.Component {
           <input
             type='text'
             name='username'
-            ref={node => this.inputNode = node}
+            onChange={this.hanleChange}
           />
         </label>
-        <button type='submit'>Submit</button>
+        {error ? <div style={{color: 'red'}}>{error}</div> : null}
+        <button disabled={Boolean(error)} type='submit'>Submit</button>
       </form>
     )
   }
@@ -35,7 +52,17 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <NameForm />
+        <NameForm
+          getErrorMessage={value => {
+            if (value.length < 3) {
+              return `Value must be > 3 characters`
+            }
+            if (!value.includes('s')) {
+              return `Value does not include 's'`
+            }
+            return null
+          }}
+        />
       </div>
     );
   }
