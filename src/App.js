c76126f66fbc4fe0736aa534d3d785cd7f9d2ba7
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-12-31 13:14:14 +0200

    17-egghead-egghead-use-the-key-prop-when-rendering-a-list-with-react

--- src/App.js
+++ src/App.js
@@ -2,113 +2,62 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class MyFancyForm extends React.Component {
-  static availableOptions = [
-    'apple',
-    'grape',
-    'cherry',
-    'orange',
-    'pear',
-    'peach',
-  ]
-  state = {multiline: '', commaSeparated: '', multiSelect: []}
-  handleCommaSeparatedChange = event => {
-    const {value} = event.target
-    const allVals = value
-      .split(',')
-      .map(v => v.trim())
-      .filter(Boolean)
-    this.setState({
-      commaSeparated: value,
-      multiline: allVals
-        .join('\n'),
-        multiSelect: allVals.filter(v => MyFancyForm.availableOptions.includes(v),),
-    })
-  }
-  handleMultilineChange = event => {
-    const {value} = event.target
-    const allVals = value
-      .split('\n')
-      .map(v => v.trim())
-      .filter(Boolean)
-    this.setState({
-      multiline: value,
-      commaSeparated: allVals
-        .join(','),
-      multiSelect: allVals.filter(v => MyFancyForm.availableOptions.includes(v),),
-    })
-  }
-  handleMultiSelectChange = event => {
-    console.log(event.target.selectedOptions);
-    const allVals = Array.from(event.target.selectedOptions).map(o => o.value,)
-    this.setState({
-      multiSelect: allVals,
-      multiline: allVals.join('\n'),
-      commaSeparated: allVals.join(','),
-    })
-  }
-  render() {
-    const {commaSeparated, multiline, multiSelect} = this.state
-    return (
-      <form>
-        <div>
-          <label>
-            comma separated values:
-            <br />
-            <input
-              type='text'
-              value={commaSeparated}
-              onChange={
-                this.handleCommaSeparatedChange
-              }
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            multiline values:
-            <br />
-            <textarea
-              value={multiline}
-              rows={MyFancyForm.availableOptions.length}
-              onChange={this.handleMultilineChange}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Multi select values:
-            <br />
-            <select
-              multiple
-              value={multiSelect}
-              size={MyFancyForm.availableOptions.length}
-              onChange={this.handleMultiSelectChange}
-            >
-            {MyFancyForm.availableOptions.map(
-              optionsValue => (
-                <option key={optionsValue} value={optionsValue}>
-                  {optionsValue}
-                </option>
-              )
-            )}
-          </select>
-          </label>
-        </div>
-      </form>
-    )
-  }
-}
 
 class App extends Component {
+  static allItems = [
+    {id: 'a', value: 'apple'},
+    {id: 'o', value: 'orange'},
+    {id: 'g', value: 'grape'},
+    {id: 'p', value: 'pear'},
+  ]
+
+  state = {items: []}
+
+  addItem = () => {
+    this.setState(({items}) => ({
+      items: [
+        ...items,
+        App.allItems.find(
+          i => !items.includes(i),
+        ),
+      ],
+    }))
+  }
+
+  removeItem = item => {
+    this.setState(({items}) => ({
+      items: items.filter(i => i !== item),
+    }))
+  }
+
   render() {
+    const {items} = this.state
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <MyFancyForm />
+        <button
+          disabled={
+            items.length >= App.allItems.length
+          }
+          onClick={this.addItem}
+          >+</button>
+          {items.map((i, index) => (
+            <div key={i.id}>
+              <button
+                onClick={() => this.removeItem(i)}
+                >-</button>
+                {i.value}:
+                <input />
+            </div>
+          ))}
+
+
+        {App.allItems.map(item => (
+          <div key={item.id}>{item.value}</div>
+        ))}
       </div>
     );
   }
